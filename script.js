7f2cb6e90e5391090e034c8ae4ed42f9a0a94cb0
vividlyyyy <vgu6@gatech.edu>
2024-08-11 12:07:43 -0700

    image popup working

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-let scrollContainer = document.querySelector(".projectBar");
-let nextArrow1 = document.getElementById("nextArrow1");
+let scrollContainer1 = document.querySelector(".projectBar");
+let scrollContainer2 = document.querySelector(".gallery");
+
 
 nextArrow1.addEventListener("click", () => {
-    scrollContainer.scrollLeft += 400;
+    scrollContainer1.scrollTo({
+        left: scrollContainer1.scrollLeft + 550,
+        behavior: "smooth"
+    });
+});
+
+backArrow1.addEventListener("click", () => {
+    scrollContainer1.scrollTo({
+        left: scrollContainer1.scrollLeft - 550,
+        behavior: "smooth"
+    });
+});
+
+nextArrow2.addEventListener("click", () => {
+    scrollContainer2.scrollTo({
+        left: scrollContainer2.scrollLeft + 500,
+        behavior: "smooth"
+    });
+});
+
+backArrow2.addEventListener("click", () => {
+    scrollContainer2.scrollTo({
+        left: scrollContainer2.scrollLeft - 500,
+        behavior: "smooth"
+    });
+});
+
+const popupContainer = document.querySelector('.popup-container');
+const popupImage = document.getElementById("popup");
+const xButton = document.getElementById("xButton");
+
+document.querySelectorAll('.gallery img').forEach(image => {
+    image.addEventListener("click", () => {
+        popupImage.src = image.getAttribute('src');
+        popupContainer.style.display = "flex";
+    });
+});
+
+xButton.addEventListener("click", () => {
+    popupContainer.style.display = "none";
 });
